7694e32e81a49ef31e969a5822d71c001fffa1ed
Smile <misobae0731@gmail.com>
2024-09-12 20:12:56 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,13 @@
+function solution(a, b, c) {
+    const sum = a + b + c;
+    const squareSum = a ** 2 + b ** 2 + c ** 2;
+    const cubeSum = a ** 3 + b ** 3 + c ** 3;
+
+    if (a === b && b === c) {
+        return sum * squareSum * cubeSum;
+    } else if (a === b || a === c || b === c) {
+        return sum * squareSum;
+    } else {
+        return sum;
+    }
+}
