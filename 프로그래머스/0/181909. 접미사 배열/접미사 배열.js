6591d6fdfa520b7cd7e38f10c9da0397c011ce90
Smile <misobae0731@gmail.com>
2024-10-06 09:12:15 +0900

    [level 0] Title: 접미사 배열, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let arr = [];
+    
+    for (let i = 0; i < my_string.length; i++) {
+        arr.push(my_string.slice(-i, my_string.length));
+    }
+
+    return arr.sort();
+}
