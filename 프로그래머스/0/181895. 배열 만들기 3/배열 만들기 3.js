748c8196d9102ffc3c4ce9a06e3148671e6bbc69
Smile <misobae0731@gmail.com>
2024-10-21 07:54:29 +0900

    [level 0] Title: 배열 만들기 3, Time: 4.02 ms, Memory: 43.9 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,6 +1,8 @@
 function solution(arr, intervals) {
-    const firstArr = arr.slice(intervals[0][0], intervals[0][1] + 1);
-    const secondArr = arr.slice(intervals[1][0], intervals[1][1] + 1);
+//     const firstArr = arr.slice(intervals[0][0], intervals[0][1] + 1);
+//     const secondArr = arr.slice(intervals[1][0], intervals[1][1] + 1);
+//     return [...firstArr, ...secondArr];
     
-    return [...firstArr, ...secondArr];
+    const [[a, b], [c, d]] = intervals;
+    return [...arr.slice(a, b + 1), ...arr.slice(c, d + 1)];
 }
