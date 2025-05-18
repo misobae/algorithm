9bad49f939fe8e305d947bd6650552c524d4e1ba
Smile <misobae0731@gmail.com>
2025-05-18 17:05:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.45 ms
+메모리: 33.5 MB, 시간: 0.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 18일 16:59:24
+2025년 05월 18일 17:05:42
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,26 +1,33 @@
-const decimalToBinary = (numbers, length) => {
-    return numbers.map((x) => x.toString(2).padStart(length, '0'))
+function solution(n, arr1, arr2) {
+  return arr1.map((num1, i) => {
+    const binary = (num1 | arr2[i]).toString(2).padStart(n, '0');
+    return binary.replace(/[01]/g, bit => (bit === '0' ? ' ' : '#'));
+  });
 }
 
-function solution(n, arr1, arr2) {
-    let result = [];
+// const decimalToBinary = (numbers, length) => {
+//     return numbers.map((x) => x.toString(2).padStart(length, '0'))
+// }
+
+// function solution(n, arr1, arr2) {
+//     let result = [];
     
-    const binaryArr1 = decimalToBinary(arr1, n);
-    const binaryArr2 = decimalToBinary(arr2, n);
+//     const binaryArr1 = decimalToBinary(arr1, n);
+//     const binaryArr2 = decimalToBinary(arr2, n);
     
-    binaryArr1.forEach((x, i) => {
-        let wholeBinaryArr = [];
+//     binaryArr1.forEach((x, i) => {
+//         let wholeBinaryArr = [];
         
-        x.split('').forEach((y, j) => {
-            if (y === '1' || binaryArr2[i][j] === '1') {
-                wholeBinaryArr.push('1');
-            } else {
-                wholeBinaryArr.push('0');
-            }
-        })
+//         x.split('').forEach((y, j) => {
+//             if (y === '1' || binaryArr2[i][j] === '1') {
+//                 wholeBinaryArr.push('1');
+//             } else {
+//                 wholeBinaryArr.push('0');
+//             }
+//         })
         
-        result.push(wholeBinaryArr.join(''));
-    })
+//         result.push(wholeBinaryArr.join(''));
+//     })
     
-    return result.map((x) => x.replaceAll(0, ' ').replaceAll(1, '#'));
-}
+//     return result.map((x) => x.replaceAll(0, ' ').replaceAll(1, '#'));
+// }
