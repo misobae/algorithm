93ef75866189fd064550cae5b3e3fac6ea992a08
Smile <misobae0731@gmail.com>
2025-05-23 23:41:45 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,13 +1,21 @@
 function solution(s) {
     const arr = s.split(' ');
-    let min = arr[0];
-    let max = arr[0];
-    
-    for (let i = 1; i < arr.length; i++) {
-        const value = Number(arr[i]);
-        if (value < min) min = value;
-        if (value > max) max = value;
-    }
-    
-    return `${min} ${max}`;
+    return Math.min(...arr) + ' ' + Math.max(...arr);
+    // 코드는 매우 간결하지만 두 번 순회하므로 O(2n)
+    // 배열의 길이가 짧을 경우에는 괜찮지만,
+    // for문 한 번으로 순회하는 것이 효율적
 }
+
+// function solution(s) {
+//     const arr = s.split(' ');
+//     let min = arr[0];
+//     let max = arr[0];
+    
+//     for (let i = 1; i < arr.length; i++) {
+//         const value = Number(arr[i]);
+//         if (value < min) min = value;
+//         if (value > max) max = value;
+//     }
+    
+//     return `${min} ${max}`;
+// }
